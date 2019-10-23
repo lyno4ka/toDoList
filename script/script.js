c7f98e3a0475111c8618bfa05c3f9e1288b1e7ab
lyno4ka <yuliyamasterenko@gmail.com>
2019-10-23 17:29:12 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,16 +1,16 @@
-let addMessage = document.querySelector('.message');
-let addButton =  document.querySelector('.add');
-let todo = document.querySelector('.todo');
+let addMessage = document.querySelector('.message'),
+ addButton =  document.querySelector('.add'),
+ todo = document.querySelector('.todo'); 
 
 let todoList = [];
 
 if(localStorage.getItem('todo')){
     todoList = JSON.parse(localStorage.getItem('todo'));
-    // displayMessages();
+    displayMessages();
 } 
 
-addButton.addEventListener('click', () => {
-
+addButton.addEventListener('click', function() {
+    if(!addMessage.value) return;
     let newTodo = {
         todo: addMessage.value,
         checked: false,
@@ -20,20 +20,46 @@ addButton.addEventListener('click', () => {
     todoList.push(newTodo); 
     displayMessages();
     localStorage.setItem('todo', JSON.stringify(todoList));
+    addMessage.value = '';
 });
 
- 
-
-let displayMessages = () => {
+function displayMessages(){
     let displayMessage = '';
-    todoList.forEach ((item, i) => {
+    if(todoList.length === 0) todo.innerHTML = '';
+    todoList.forEach (function(item, i){
         displayMessage += `
         <li>
-        <input type="checkbox" id="item_${i}" ${item.checked ? 'checked' : ''}>
-        <label for="item_${i}">${item.todo}</label>
+            <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+            <label for='item_${i}' class='${item.important ? 'important' : ''}'>${item.todo}</label>
         </li>
         `;
         todo.innerHTML = displayMessage;
     });
-
 };
+
+todo.addEventListener('change', function(event){
+    let idInput = event.target.getAttribute('id');
+    let valueLabel = todo.querySelector('[for='+ idInput +']').innerHTML;
+    
+    todoList.forEach(function(item){
+        if(item.todo === valueLabel) {
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
+});
+
+todo.addEventListener('contextmenu', function(event){
+    event.preventDefault();
+    todoList.forEach(function(item, i){
+        if(item.todo === event.target.innerHTML) {
+            if(event.ctrlKey || event.metaKey){
+                todoList.splice(i, 1);
+            } else {
+               item.important = !item.important; 
+            }
+            displayMessages();
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
+});
